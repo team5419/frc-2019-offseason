219c8dbd14f936942ed5b60d7436328defbfa552
Nicholas <nachos.underwood@gmail.com>
2019-09-25 06:28:53 -0700

    redementry vacuum

--- src/main/kotlin/org/team5419/frc2019offseason/subsystems/Vacuum.kt
+++ src/main/kotlin/org/team5419/frc2019offseason/subsystems/Vacuum.kt
@@ -2,8 +2,37 @@ package org.team5419.frc2019offseason.subsystems
 
 import org.team5419.fault.Subsystem
 
-class Vacuum() : Subsystem() {
-    public override fun update() {}
-    public override fun stop() {}
-    public override fun reset() {}
+import edu.wpi.first.wpilibj.Solenoid
+import org.team5419.fault.hardware.LazyTalonSRX
+import com.ctre.phoenix.motorcontrol.ControlMode
+
+class Vacuum(
+    masterTalon: LazyTalonSRX,
+    solnoid: Solenoid
+) : Subsystem() {
+
+    private val mMaster: LazyTalonSRX
+    private val mSolenoid: Solenoid
+    private var isPumping: Boolean = false
+
+    init {
+        mMaster = masterTalon
+        mSolenoid = solnoid
+    }
+
+    public fun setValve(value: Boolean) {
+        mSolenoid.set(value)
+    }
+
+    public override fun update() {
+        if (isPumping) mMaster.set(ControlMode.PercentOutput, 1.0)
+        else mMaster.set(ControlMode.PercentOutput, 0.0)
+    }
+
+    public override fun stop() {
+        isPumping = false
+        mMaster.set(ControlMode.PercentOutput, 0.0)
+        mSolenoid.set(false)
+    }
+    public override fun reset() { stop() }
 }
