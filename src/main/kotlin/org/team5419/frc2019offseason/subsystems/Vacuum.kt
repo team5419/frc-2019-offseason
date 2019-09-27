d13cb58a28c74303730aea7c816bf2a0be673115
Nicholas Underwood <nachos.underwood@gmail.com>
2019-09-27 00:18:40 +0000

    Dev/vacuum (#3)
    * redementry vacuum
    
    * Vacuum control scheme
    
    * fixed problem
    
    * vacuum itermitit retainer

--- src/main/kotlin/org/team5419/frc2019offseason/subsystems/Vacuum.kt
+++ src/main/kotlin/org/team5419/frc2019offseason/subsystems/Vacuum.kt
@@ -5,28 +5,40 @@ import org.team5419.fault.Subsystem
 import edu.wpi.first.wpilibj.Solenoid
 import org.team5419.fault.hardware.LazyTalonSRX
 import com.ctre.phoenix.motorcontrol.ControlMode
+import edu.wpi.first.wpilibj.Timer
 
 class Vacuum(
     masterTalon: LazyTalonSRX,
     solnoid: Solenoid
 ) : Subsystem() {
+    private val mTimer: Timer = Timer()
 
     private val mTalon: LazyTalonSRX
     private val mSolenoid: Solenoid
     public var pump: Boolean = false
+    private var suck: Boolean = false
 
     init {
         mTalon = masterTalon
         mSolenoid = solnoid
     }
 
-    public fun setValve(value: Boolean) = mSolenoid.set(value)
+    public fun setValve(value: Boolean) {
+        mSolenoid.set(value)
+        suck = value
+        if (value) mTimer.start()
+        else {
+            mTimer.stop()
+            mTimer.reset()
+        }
+    }
 
     public fun toogleValve() = setValve(!mSolenoid.get())
 
     public fun setPercent(percent: Double) = mTalon.set(ControlMode.PercentOutput, percent)
 
     public override fun update() {
+        pump = pump || (suck && mTimer.get().toInt() % 2 == 1)
         if (pump) mTalon.set(ControlMode.PercentOutput, 1.0)
         else mTalon.set(ControlMode.PercentOutput, 0.0)
         pump = false
